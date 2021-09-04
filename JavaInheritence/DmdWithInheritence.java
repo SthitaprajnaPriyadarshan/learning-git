89f5ef591770ef3aecfeff871f041eac6c5fd51e
SthitaprajnaPriyadarshan <sthitaprajna2014@gmail.com>
2021-09-05 01:28:51 +0530

    CCEP:1 Added directory JavaInheritence, Added file DmdWithInheritence, put code in it.
    The first 3 changes were made in a single commit

---
+++ JavaInheritence/DmdWithInheritence.java
@@ -0,0 +1,36 @@
+interface Red {
+
+	void foo();
+
+	void fun();
+}
+
+abstract class Blue implements Red{
+	public void fun() {
+		System.out.println("Blue / fun()\n ");
+	}
+}
+
+class Violet extends Blue implements Red{
+
+	@Override
+	public void foo() {
+		System.out.println("Red->Violet / foo()");
+	}
+}
+
+public class DmdWithInheritence {
+	public static void main(String args[]) {
+		Violet V = new Violet();
+		V.foo();
+		V.fun();
+
+		Red R = new Violet();
+		R.foo();
+		R.fun();
+
+		Blue B = new Violet();
+		B.foo();
+		B.fun();
+	}
+}
